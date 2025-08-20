7436796b56683bbe73c6f1c4b685b510b577d846
Carina Dalpino <carina.g.santos22@gmail.com>
2025-08-20 19:09:55 -0300

    atualização

--- primeiro_script.py
+++ primeiro_script.py
@@ -1,4 +1,6 @@
-# Meu primeiro programanome = 
-print("Olá, mundo!")
-print("Meu nome é {nome}")
-print("Minha idade é {idade} anos ")
+
+Nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+profissao = input("Digite sua profissão: ")
+
+print('Olá, meu nome é', Nome, ', tenho', idade, 'anos e sou', profissao + '.')
