66cb2283dc24ec27134a125888e6fd91b0e4ed10
Carina Dalpino <carina.g.santos22@gmail.com>
2025-08-25 13:21:23 -0300

    Exercicios Aula 02

--- primeiro_script.py
+++
@@ -1,6 +0,0 @@
-
-Nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-profissao = input("Digite sua profissão: ")
-
-print('Olá, meu nome é', Nome, ', tenho', idade, 'anos e sou', profissao + '.')
